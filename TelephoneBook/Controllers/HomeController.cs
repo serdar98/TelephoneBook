fcd75d41d547a4699228377e749b5af2f54f81ff
Serdar EREN <34771706+serdar98@users.noreply.github.com>
2020-01-30 18:14:39 +0300

    Add project files.

---
+++ TelephoneBook/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelephoneBook.Models;
+using System.Data.SqlClient;
+namespace TelephoneBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TelephoneBookEntities db = new TelephoneBookEntities();
+        
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(loginAdmin objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                {
+                    var obj = db.loginAdmin.Where(a => a.adminUsername.Equals(objUser.adminUsername) && a.adminPassword.Equals(objUser.adminPassword)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["adminUserID"] = obj.adminUserID.ToString();
+                        Session["adminUsername"] = obj.adminUsername.ToString();
+                        return RedirectToAction("Index","Info");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("adminUsername", "Kullanıcı Adınız Yanlış");
+                        ModelState.AddModelError("adminPassword", "Şifreniz Yanlış");
+                        return View("Login");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StaffLogin(StaffLogin objStaff)
+        {
+            if (ModelState.IsValid)
+            {
+
+                {
+                    var obj = db.StaffLogin.Where(a => a.staffUsername.Equals(objStaff.staffUsername) && a.staffPassword.Equals(objStaff.staffPassword)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["staffUserID"] = obj.staffUserID.ToString();
+                        Session["staffUsername"] = obj.staffUsername.ToString();
+                        return RedirectToAction("PublicUI","Info");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("staffUsername", "Kullanıcı Adınız Yanlış");
+                        ModelState.AddModelError("staffPassword", "Şifreniz Yanlış");
+                        return View("StaffLogin");
+                    }
+                }
+            }
+            return View(objStaff);
+        }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult StaffLogin()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TelephoneBook/Controllers/InfoController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelephoneBook.Models;
+namespace TelephoneBook.Controllers
+{
+    public class InfoController : Controller
+    {
+        TelephoneBookEntities db = new TelephoneBookEntities();
+        public static int idHolder = 0;
+        public ActionResult Index(string p)
+        {
+            var value = from v in db.StaffInformation select v;
+            if (!string.IsNullOrEmpty(p))
+            {
+                value = value.Where(m=>m.Name.Contains(p));
+            }
+            return View(value.ToList());
+            //var info = db.StaffInformation.ToList();
+            //return View(info);
+        }
+
+        public ActionResult PublicUI()
+        {
+            var info2 = db.StaffInformation.ToList();
+            return View(info2);
+        }
+
+        [HttpGet]
+        public ActionResult AddStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStaff(StaffInformation staff)
+        {
+
+            db.StaffInformation.Add(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Info");
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var info = db.StaffInformation.Find(id);
+            db.StaffInformation.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Info");
+        }
+
+        public ActionResult Update(int id)
+        {
+            idHolder = id;
+            var update = db.StaffInformation.Find(id);
+
+            return View("Update",update);
+        }
+
+        public ActionResult Update2(StaffInformation staffUpdate)
+        {
+            staffUpdate.Staff_ID = idHolder;
+            var update = db.StaffInformation.Find(idHolder);
+            update.Name = staffUpdate.Name;
+            update.Surname = staffUpdate.Surname;
+            update.Telephone = staffUpdate.Telephone;
+            update.Department = staffUpdate.Department;
+            update.Manager = staffUpdate.Manager;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Info");
+        }
+
+    }
+}
